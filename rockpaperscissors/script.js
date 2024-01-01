c2ead41cf88fbc14173838ecf68c910bfbb18b92
wilyJ80 <abjurandam@gmail.com>
2023-12-31 22:28:32 -0300

    Finish Rock Paper Scissors game

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -21,8 +21,7 @@ function playRound(playerSelection, computerSelection) {
 	if (rockRegex.test(playerSelection)) {
 
 		if (rockRegex.test(computerSelection)) {
-			// TIE: replay the round
-			playRound(playerSelection, computerSelection);
+			return "TIE!";
 
 		} else if (paperRegex.test(computerSelection)) {
 			return "You lose! Paper beats Rock!";
@@ -37,10 +36,9 @@ function playRound(playerSelection, computerSelection) {
 
 		if (rockRegex.test(computerSelection)) {
 			return "You win! Paper beats Rock!";
-			
+
 		} else if (paperRegex.test(computerSelection)) {
-			// TIE: replay the round
-			playRound(playerSelection, computerSelection);
+			return "TIE!";
 
 		} else if (scissorsRegex.test(computerSelection)) {
 			return "You lose! Scissors beat Paper!";
@@ -48,7 +46,7 @@ function playRound(playerSelection, computerSelection) {
 	}
 
 	// Regex test: player chooses scissors
-	if (scissorsRegex.test(computerSelection)) {
+	if (scissorsRegex.test(playerSelection)) {
 
 		if (rockRegex.test(computerSelection)) {
 			return "You lose! Rock beats Scissors!";
@@ -57,11 +55,44 @@ function playRound(playerSelection, computerSelection) {
 			return "You win! Scissors beats Paper!";
 
 		} else if (scissorsRegex.test(computerSelection)) {
-			// TIE: replay the round
-			playRound(playerSelection, computerSelection);
+			return "TIE!";
 		}
 	}
-	
+
 	// Player input did not match any of the valid options
 	console.log("Matching error: player input not valid!");
 }
+
+function game() {
+
+	let playerWins = 0, computerWins = 0;
+	let count = 0;
+	while (count < 5) {
+		alert("Round " + (count + 1));
+
+		let playerChoice = prompt("Rock, Paper, Scissors?");
+		let computerChoice = getComputerChoice();
+
+		let result = playRound(playerChoice, computerChoice);
+		alert(result);
+
+		if (result.startsWith("You win")) {
+			playerWins++;
+			count++;
+
+		} else if (result.startsWith("You lose")) {
+			computerWins++;
+			count++;
+		}
+	}
+
+	// Final game outcome
+	if (playerWins > computerWins) {
+		alert("Congrats! You win the game!");
+
+	} else {
+		alert("Sorry. The machine wins. Try again.");
+	}
+}
+
+game();
