d7c47a40bafc2a21b832c778c747077f9133feb8
wilyJ80 <abjurandam@gmail.com>
2024-01-08 18:05:24 -0300

    Hovering changing box colors, finished

--- etchasketch/script.js
+++ etchasketch/script.js
@@ -7,5 +7,16 @@ for (let i = 0; i < 16; i++) {
 }
 
 // 1. Set up hover effect event on divs
+const boxes = document.querySelectorAll(".box");
+
+for (const box of boxes) {
+	box.addEventListener("mouseenter", () => {
+		box.style.background = "lightblue";
+	});
+
+	box.addEventListener("mouseleave", () => {
+		box.style.background = "";
+	});
+}
 
 // 2. Set up event: leave pixelated trail on divs
