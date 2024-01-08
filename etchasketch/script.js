7216d7ba383e5d3ccd5496a38612d7fd0177b5dd
wilyJ80 <abjurandam@gmail.com>
2024-01-08 17:01:16 -0300

    etch-a-sketch: add correctly sized boxes

---
+++ etchasketch/script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+	const content = document.createElement("div");
+	content.classList.add("content");
+	container.appendChild(content);
+}
