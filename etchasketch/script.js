7d7c2c5ea87dfcb18eaf80ca550d8e51cd658ec1
wilyJ80 <abjurandam@gmail.com>
2024-01-09 19:31:00 -0300

    Finish coloring feature

--- etchasketch/script.js
+++ etchasketch/script.js
@@ -6,23 +6,10 @@ for (let i = 0; i < 16; i++) {
 	container.appendChild(box);
 }
 
-// 1. Set up hover effect event on divs
 const boxes = document.querySelectorAll(".box");
 
 for (const box of boxes) {
-	box.addEventListener("mouseenter", () => {
+	box.addEventListener("mouseover", () => {
 		box.style.background = "lightblue";
 	});
-
-	box.addEventListener("mouseleave", () => {
-		box.style.background = "";
-	});
 }
-
-// 2. Set up event: leave pixelated trail on divs
-// How to leave pixelated trail on divs:
-// 2.1 mousemove event listener?
-// 2.2 get correct mouse coordinates at div
-// 2.3 create dot div
-// 2.4 adjust its dimensions
-// 2.5 append this child
