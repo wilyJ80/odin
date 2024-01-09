9cf26e230b4434c994e9d0bd76cac02624e3f6a8
wilyJ80 <abjurandam@gmail.com>
2024-01-09 20:11:11 -0300

    Add button to top

--- etchasketch/script.js
+++ etchasketch/script.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 16; i++) {
 	const row = document.createElement("div");
+	row.classList.add("row");
 
 	for (let j = 0; j < 16; j++) {
 		const box = document.createElement("div");
@@ -19,3 +20,7 @@ for (const box of boxes) {
 		box.style.backgroundColor = "#00f";
 	});
 }
+
+const button = document.createElement("button");
+button.textContent = "Generate";
+container.appendChild(button);
