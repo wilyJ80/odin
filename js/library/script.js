a5390eb18ab7ee672c15e140bffb5d8c49ada1d5
wilyJ80 <abjurandam@gmail.com>
2024-02-12 20:54:49 -0300

    Finish Step 2

--- js/library/script.js
+++ js/library/script.js
@@ -1,12 +1,23 @@
 const myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
     this.read = false;
+
+    function info() {
+        const info = `${title} by ${author}, ${pages} pages, `;
+
+        if (read) {
+            info += 'already read.';
+        } else {
+            info += 'not read yet.';
+        }
+    }
 }
 
-function addBookToLibrary(book, myLibrary) {
+function addBookToLibrary(book) {
     myLibrary.push(book);
-    return myLibrary;
-}
+} 
+
