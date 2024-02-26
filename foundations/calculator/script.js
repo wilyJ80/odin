cd88a4994e55101e5247cf58168fd0155261e557
wilyJ80 <abjurandam@gmail.com>
2024-02-26 13:30:43 -0300

    Buggy calculator

--- foundations/calculator/script.js
+++ foundations/calculator/script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-	return a + b;
+	return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
@@ -29,12 +29,25 @@ function operate(operator, a, b) {
 	}
 }
 
-let a;
-let b;
-let operator;
+function toPrefixNotation(arr) {
+	const tmp = arr[1];
+	arr[1] = arr[0];
+	arr[0] = tmp;
+}
+
+function prefixCalc(arr) {
+	if (arr.length == 2) {
+		toPrefixNotation(arr);
+	} else {
+		const result = operate(arr[0], arr[1], arr[2]);
+		arr.splice(0, 3, result);
+		
+	}
+}
 
 let display = document.querySelector(".display");
 let displayValue = "";
+let dataStack = [];
 
 document.querySelectorAll(".number").forEach((button) => {
 	button.addEventListener("click", () => {
@@ -43,3 +56,24 @@ document.querySelectorAll(".number").forEach((button) => {
 	});
 }); 
 
+document.querySelectorAll('.operation').forEach((button) => {
+	button.addEventListener("click", () => {
+
+		dataStack.push(displayValue);
+		const operation = button.textContent;
+		dataStack.push(operation);
+
+		if (operation === '=') {
+			dataStack.pop();
+			prefixCalc(dataStack);
+
+			display.textContent = dataStack[0];
+			displayValue = 0;
+
+		} else {
+			prefixCalc(dataStack);
+			display.textContent = "0";
+			displayValue = 0;
+		}
+	});
+});
