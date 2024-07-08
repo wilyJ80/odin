c4457aa635ea324622102d97bc3fa970bf994dbc
wilyJ80 <abjurandam@gmail.com>
2024-07-08 14:54:31 -0300

    Finish game logic

--- js/tictactoe/game.js
+++ js/tictactoe/game.js
@@ -10,10 +10,48 @@ export function createGame(player1, player2) {
 		['', '', ''],
 		['', '', '']
 	];
-	const player1Turn = (x, y) => positions[x][y] = player1.marker;
-	const player2Turn = (x, y) => positions[x][y] = player2.marker;
 
-	return { player1Turn, player2Turn, positions };
+	const isPositionTaken = (x, y) => positions[x][y] !== '';
+
+	const player1Turn = (x, y) => {
+		if (!isPositionTaken(x, y)) {
+			positions[x][y] = player1.marker;
+		}
+	};
+	const player2Turn = (x, y) => {
+		if (!isPositionTaken(x, y)) {
+			positions[x][y] = player2.marker;
+		}
+	};
+
+	const winningCombinations = [
+		// Rows
+		[[0, 0], [0, 1], [0, 2]],
+		[[1, 0], [1, 1], [1, 2]],
+		[[2, 0], [2, 1], [2, 2]],
+		// Columns
+		[[0, 0], [1, 0], [2, 0]],
+		[[1, 0], [1, 1], [1, 2]],
+		[[2, 0], [2, 1], [2, 2]],
+		// Diagonals
+		[[0, 0], [1, 1], [2, 2]],
+		[[0, 2], [1, 1], [2, 0]]
+
+	];
+
+	const checkWin = () => {
+		if (winningCombinations.some(combination =>
+			combination.every(([x, y]) => positions[x][y] === 'O'))) {
+			return 1;
+		} else if (winningCombinations.some(combination =>
+			combination.every(([x, y]) => positions[x][y] === 'O'))) {
+			return 2;
+		} else {
+			return 0;
+		}
+	}
+
+	return { player1Turn, player2Turn, positions, checkWin };
 }
 
 // create players
--- js/tictactoe/game.test.js
+++ js/tictactoe/game.test.js
@@ -14,7 +14,7 @@ test('turn works', () => {
 	]);
 });
 
-test('turn works', () => {
+test('turn works in another position', () => {
 	const game = createGame(player1, player2);
 	game.player1Turn(0, 1);
 	expect(game.positions).toEqual([
@@ -23,3 +23,51 @@ test('turn works', () => {
 		['', '', '']
 	]);
 });
+
+test('turn works with two players', () => {
+	const game = createGame(player1, player2);
+	game.player1Turn(0, 1);
+	game.player2Turn(0, 0);
+	expect(game.positions).toEqual([
+		['X', 'O', ''],
+		['', '', ''],
+		['', '', '']
+	]);
+});
+
+test('overwriting is not allowed', () => {
+	const game = createGame(player1, player2);
+	game.player1Turn(0, 1);
+	game.player2Turn(0, 0);
+	game.player1Turn(0, 0);
+	expect(game.positions).toEqual([
+		['X', 'O', ''],
+		['', '', ''],
+		['', '', '']
+	]);
+});
+
+test('winning condition', () => {
+	const game = createGame(player1, player2);
+	game.player1Turn(0, 0);
+	game.player1Turn(1, 1);
+	game.player1Turn(2, 2);
+	expect(game.checkWin(player1.marker)).toBe(1);
+});
+
+// test.todo('draw');
+test('draw', () => {
+	const game = createGame(player1, player2);
+	game.player1Turn(0, 0);
+	game.player1Turn(0, 2);
+	game.player1Turn(1, 2);
+	game.player1Turn(2, 1);
+	game.player2Turn(0, 1);
+	game.player2Turn(1, 0);
+	game.player2Turn(1, 1);
+	game.player2Turn(2, 0);
+	game.player2Turn(2, 2);
+	expect(game.checkWin()).toBe(0);
+});
+
+test.todo('test full game loop');
