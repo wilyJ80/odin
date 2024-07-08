60a45e6365c440c759ab9a20b5a2e05a249ed5ab
wilyJ80 <abjurandam@gmail.com>
2024-07-08 13:57:24 -0300

    Remove board object

--- js/tictactoe/game.js
+++ js/tictactoe/game.js
@@ -4,26 +4,21 @@ export function createPlayer(name, marker) {
 	return { name, marker };
 }
 
-export function createBoard() {
+export function createGame(player1, player2) {
 	const positions = [
 		['', '', ''],
 		['', '', ''],
 		['', '', '']
 	];
-	return { positions };
-}
+	const player1Turn = (x, y) => positions[x][y] = player1.marker;
+	const player2Turn = (x, y) => positions[x][y] = player2.marker;
 
-export function createGame(player1, player2, board) {
-	const player1Turn = (x, y) => board.positions[x][y] = player1.marker;
-	const player2Turn = (x, y) => board.positions[x][y] = player2.marker;
-
-	return { player1Turn, board };
+	return { player1Turn, player2Turn, positions };
 }
 
 // create players
 const player1 = createPlayer('Player 1', 'O');
 const player2 = createPlayer('Player 2', 'X');
-const board = createBoard();
 // gameloop: players take turns
 // player 1 fills a position
 // player 2 fills another
--- js/tictactoe/game.test.js
+++ js/tictactoe/game.test.js
@@ -1,14 +1,13 @@
 import { expect, test } from 'vitest';
-import { createPlayer, createBoard, createGame } from './game';
+import { createPlayer, createGame } from './game';
 
 const player1 = createPlayer('Player 1', 'O');
 const player2 = createPlayer('Player 2', 'X');
 
 test('turn works', () => {
-	const board = createBoard(player1, player2);
-	const game = createGame(player1, player2, board);
+	const game = createGame(player1, player2);
 	game.player1Turn(0, 0);
-	expect(game.board.positions).toEqual([
+	expect(game.positions).toEqual([
 		['O', '', ''],
 		['', '', ''],
 		['', '', '']
@@ -16,10 +15,9 @@ test('turn works', () => {
 });
 
 test('turn works', () => {
-	const board = createBoard(player1, player2);
-	const game = createGame(player1, player2, board);
+	const game = createGame(player1, player2);
 	game.player1Turn(0, 1);
-	expect(game.board.positions).toEqual([
+	expect(game.positions).toEqual([
 		['', 'O', ''],
 		['', '', ''],
 		['', '', '']
