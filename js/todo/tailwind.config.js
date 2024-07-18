2bdf4962094afde5a877c2cf78b3b6a2b152169b
wilyJ80 <abjurandam@gmail.com>
2024-07-18 19:23:56 -0300

    Setup Daisyui project

---
+++ js/todo/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
