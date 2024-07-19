d303a0b35df94cb7338ec14ddce9603a28a5eb39
wilyJ80 <abjurandam@gmail.com>
2024-07-19 20:39:44 -0300

    Add webpack bundle analyzer

--- js/todo/webpack.config.js
+++ js/todo/webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -25,6 +26,7 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
+        new BundleAnalyzerPlugin(),
 
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
