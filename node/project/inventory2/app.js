3735ed5255dda8025e2ce8f014f2a1f6894f7150
wilyJ80 <abjurandam@gmail.com>
2024-12-24 18:40:58 -0300

    chore: setup nodemon and tailwind

--- node/project/inventory2/app.js
+++ node/project/inventory2/app.js
@@ -3,9 +3,14 @@ import ejs from 'ejs';
 
 export const app = express();
 
+// use ejs as template engine
 app.set('view engine', 'ejs');
+// render .html files with ejs
 app.engine('html', ejs.renderFile);
+// parse request body
 app.use(express.urlencoded({ extended: true }));
+// serve static files (tailwind requires)
+app.use(express.static('views'));
 
 app.get('/', (req, res) => {
 	res.render('index.html');
---
+++ node/project/inventory2/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
