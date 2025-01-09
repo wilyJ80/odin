daf4263a903e0558d42276288fd8b7018dae447c
wilyJ80 <abjurandam@gmail.com>
2025-01-09 11:04:17 -0300

    fix: transformed rows, todo: views

--- node/project/inventory2/controller/indexController.js
+++ node/project/inventory2/controller/indexController.js
@@ -6,12 +6,39 @@ import { selectAllClassesAndStudents } from '../db/classQueries.js';
  */
 export const getIndex = async (req, res) => {
 	const results = await selectAllClassesAndStudents();
-	const grouped = Object.groupBy(results, ({ class_name }) => class_name);
-	const allClasses = Object.entries(grouped).map(([class_name, students]) => ({
-		class_name,
-		students: students.map(({ student_name }) => student_name)
-	}));
+	//console.log(results);
+	//const grouped = Object.groupBy(results, ({ class_name }) => class_name);
+	//const allClasses = Object.entries(grouped).map(([class_name, students]) => ({
+	//	class_name,
+	//	students: students.map(({ student_name }) => student_name)
+	//}));
+	const transformed = transformRows(results);
+	console.log(transformed);
 	res.render('index.html', {
-		allClasses: allClasses
+		allClasses: transformed
 	});
 };
+
+/** @param {Array<Object>} rows */
+const transformRows = (rows) => {
+	const result = [];
+
+	for (const row of rows) {
+		let classEntry = result.find(item => item.class.class_id === row.class_id);
+
+		if (!classEntry) {
+			classEntry = {
+				class: { class_id: row.class_id, class_name: row.class_name },
+				students: [],
+			};
+			result.push(classEntry);
+		}
+
+		classEntry.students.push({
+			student_id: row.student_id,
+			student_name: row.student_name,
+		});
+	}
+
+	return result;
+}
--- node/project/inventory2/db/classQueries.js
+++ node/project/inventory2/db/classQueries.js
@@ -5,6 +5,6 @@ import { pool } from './pool.js';
  * @param {import('express').Response} res
  */
 export const selectAllClassesAndStudents = async () => {
-	const { rows } = await pool.query('SELECT c.class_name, s.student_name FROM students_classes sc JOIN classes c ON c.class_id = sc.class_id JOIN students s ON s.student_id = sc.student_id ORDER BY c.class_name, s.student_name');
+	const { rows } = await pool.query('SELECT c.class_id, c.class_name, s.student_id, s.student_name FROM students_classes sc JOIN classes c ON c.class_id = sc.class_id JOIN students s ON s.student_id = sc.student_id ORDER BY c.class_name, s.student_name');
 	return rows;
 };
