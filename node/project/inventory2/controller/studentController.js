7768bca56ab9ad9e33af0e6cf6cbd3f8e430bdda
wilyJ80 <abjurandam@gmail.com>
2025-01-10 10:57:06 -0300

    i need a better project idea

--- node/project/inventory2/controller/studentController.js
+++ node/project/inventory2/controller/studentController.js
@@ -1,3 +1,9 @@
-export const addStudent = () => {
+import { insertStudent } from '../db/studentQueries.js';
 
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+export const addStudent = (req, res) => {
+	const { name } = req.body;
 }
--- node/project/inventory2/db/studentQueries.js
+++ node/project/inventory2/db/studentQueries.js
@@ -4,7 +4,16 @@ import { pool } from './pool.js';
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
-export const insertStudent = async (student, schoolClass) => {
-	await pool.query('INSERT INTO students (student_name) VALUES ($1)');
-	// TODO: too tired!
+export const insertStudent = async (student_name, student_id, class_id) => {
+	const client = await pool.connect();
+	try {
+		await client.query('BEGIN');
+		await client.query('INSERT INTO students (student_name) VALUES ($1)', [student_name]);
+		await client.query('INSERT INTO students_classes (student_id, class_id) VALUES ($1, $2)', [student_id, class_id]);
+		await client.query('COMMIT');
+	} catch (error) {
+		await client.query('ROLLBACK');
+	} finally {
+		client.release();
+	}
 }
