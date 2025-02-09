d27feeaaf61dbe95a6936a21fc6c1f1ee015c6a2
wilyJ80 <abjurandam@gmail.com>
2025-02-09 11:42:59 -0300

    feat: better schema

--- node/project/members/db/schema.sql
+++ node/project/members/db/schema.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS clubs;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS creators;
+DROP TABLE IF EXISTS admins;
+
 CREATE TABLE messages (
 		message_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 		message_title VARCHAR(16) NOT NULL,
@@ -7,6 +13,12 @@ CREATE TABLE messages (
 		FOREIGN KEY (member_id) REFERENCES members (member_id)
 );
 
+CREATE TABLE clubs (
+		club_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		club_name VARCHAR(32) NOT NULL,
+		club_key VARCHAR(32) NOT NULL
+);
+
 CREATE TABLE members (
 		member_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 		member_firstname VARCHAR(32) NOT NULL,
@@ -14,6 +26,13 @@ CREATE TABLE members (
 		member_email VARCHAR(32) NOT NULL,
 		password VARCHAR (255) NOT NULL,
 		salt VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE members_clubs (
+		member_id INTEGER NOT NULL,
+		club_id INTEGER NOT NULL,
+		FOREIGN KEY(member_id) REFERENCES members(member_id),
+		FOREIGN KEY(club_id) REFERENCES clubs(club_id)
 );
 
 CREATE TABLE creators (
